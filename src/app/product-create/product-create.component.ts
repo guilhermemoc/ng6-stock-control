8c9907ee35c90f18407be7c8c80d02ed302c59b8
Guilherme Martins - GMA <guilherme.martins@iteris.local>
2018-08-02 13:47:19 -0300

    .

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/service/api/api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent implements OnInit {
+export class BookCreateComponent implements OnInit {
 
-  constructor() { }
+  bookForm: FormGroup;
+  isbn = '';
+  title = '';
+  description = '';
+  author = '';
+  publisher = '';
+  published_year = '';
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.bookForm = this.formBuilder.group({
+      'isbn' : [null, Validators.required],
+      'title' : [null, Validators.required],
+      'description' : [null, Validators.required],
+      'author' : [null, Validators.required],
+      'publisher' : [null, Validators.required],
+      'published_year' : [null, Validators.required]
+    });
   }
 
+  onFormSubmit(form: NgForm) {
+    this.api.postBook(form)
+      .subscribe(res => {
+          const id = res['_id'];
+          this.router.navigate(['/book-details', id]);
+        }, (err) => {
+          console.log(err);
+        });
+  }
 }
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from 'src/app/service/api/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+export class BookDetailComponent implements OnInit {
+
+  book = {};
+
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
 
   ngOnInit() {
+    this.getBookDetails(this.route.snapshot.params['id']);
+  }
+
+  getBookDetails(id) {
+    this.api.getBook(id)
+      .subscribe(data => {
+        console.log(data);
+        this.book = data;
+      });
+  }
+
+  deleteBook(id) {
+    this.api.deleteBook(id)
+      .subscribe(res => {
+          this.router.navigate(['/books']);
+        }, (err) => {
+          console.log(err);
+        }
+      );
   }
 
 }
